330920a85c1fbe3a1e5b397915d65994a90ea6db
Dan Oneata <dan.oneata@gmail.com>
2023-12-20 16:39:22 +0200

    FIX Datasets on which to run detection

--- dolos/methods/xception_attention/evaluate.py
+++ dolos/methods/xception_attention/evaluate.py
@@ -11,7 +11,10 @@ from dolos.methods.patch_forensics.evaluate import evaluate_detection, evaluate_
 def main(supervision, train_config_name, dataset_name, to_visualize=False):
     method_name = "xception-attention"
     print(supervision, train_config_name)
-    if supervision == "weak" and dataset_name == "repaint-p2":
+    if supervision == "weak" and dataset_name in {
+        "repaint-clean",
+        "repaint-p2-celebahq-clean",
+    }:
         evaluate_detection(method_name, supervision, train_config_name, dataset_name, to_visualize)
     evaluate_localisation(method_name, supervision, train_config_name, dataset_name, to_visualize)
     print()
